0fd22dba2bb21efa8daecea50974e9c29efc0d1a
FlexxN1 <FlexxN1@users.noreply.github.com>
2022-11-03 03:50:25 +0000

    deploy: eae9b7b5ea712fdc0a1c79f9e1abad9b86ea31b2

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,4 +3,54 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+//---------------------------------------------------------------
+//Reto hecho usando async/await
+/*const url = "https://platzi-avo.vercel.app/api/avo";
+//web api
+async function fetchData() {
+  const response = await fetch(url),
+  data = await response.json(),
+  allItems = [];
+
+  data.data.forEach((item) => {
+    // create image
+    const image = document.createElement("img");
+    // create title
+    const title = document.createElement("h2");
+    // create price
+    const price = document.createElement("div");
+
+    const container = document.createElement("div");
+    container.append(image, title, price);
+
+    allItems.push(container);
+  });
+
+  document.body.append(...allItems)
+}
+
+fetchData();*/
+
+
+//-------------------------------------------
+//con promesas
+
+const url = "https://platzi-avo.vercel.app/api/avo";
+
+window.fetch(url)
+    .then(response => response.json())
+    .then((responseJson) => {
+        const allItems = []
+        responseJson.data.forEach(item => {
+            const imagen = document.createElement("img");
+            const title = document.createElement("h2");
+            const price = document.createElement("div");
+
+            const container = document.createElement("div")
+            container.append(imagen, title, price);
+
+            allItems.push(container)
+        });
+
+        document.body.append(...allItems);
+    });
