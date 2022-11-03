1b059b086615b1598c944154a6c5c0e0c62ef74d
Flexx <juandavidx40@gmail.com>
2022-11-03 14:05:15 -0500

    add [Intl] and Tailwind

--- src/index.js
+++ src/index.js
@@ -44,27 +44,64 @@ fetchData();*/
 const urlBase = "https://platzi-avo.vercel.app";
 const appNode = document.getElementById("app");
 
+
+// internacionalizacion/Intl
+ // 1.- Format fechas
+ // 2.- Format monedas
+
+const formatPrice = (price) => {
+  const newPrice = new window.Intl.NumberFormat("en-EN", {
+    style: "currency",
+    currency: "USD"
+  }).format(price)
+
+  return newPrice;
+}
+
 window
     .fetch(`${urlBase}/api/avo`)
     .then(response => response.json())
     .then((responseJson) => {
+
+        //almacenamos todo el proceso en memoria de JS para no renderizar el dom por cada iteracion
         const allItems = []
         responseJson.data.forEach(item => {
+            // Crear imagen
             const imagen = document.createElement("img");
+            imagen.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6";
             imagen.src = `${urlBase}${item.image}`;
-
+            
+            // Crear título
             const title = document.createElement("h2");
+            title.className = "text-lg"
             title.textContent = item.name;
 
+            // Crear precio
             const price = document.createElement("div");
-            price.textContent = item.price;
+            price.className = "text-gray-600"
+            price.textContent = formatPrice(item.price);
 
-            const container = document.createElement("div")
-            container.append(imagen, title, price);
+            // Creamos un contenedor el título y el precio
+            const priceAndTitle = document.createElement("div")
+            priceAndTitle.className = "text-center md:text-left";
+            priceAndTitle.appendChild(title);
+            priceAndTitle.appendChild(price);
 
-            allItems.push(container)
+            // Metemos todo dentro de una tarjeta contenedora
+            const card = document.createElement("div");
+            card.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300";
+            card.append(imagen, priceAndTitle);
+
+
+            // Metemos todo dentro del contenedor principal
+            const contenedor = document.createElement("div");
+            contenedor.appendChild(card);
+
+            //ahora lo guardamos en la memoria de JS
+            allItems.push(contenedor)
         });
 
+        //ahora ya una vez terminado el proceso, ya podemos inyectar el resultado de los elementos al body
         appNode.append(...allItems);
     })
     .catch(err => console.log(`Ha ocurrido un error de tipo: ${err}`));
